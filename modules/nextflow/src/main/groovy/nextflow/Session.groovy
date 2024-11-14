15dfe2b3f5f062b5ab3a46655a30713e6571ae17
cv-guillermo <138443719+u6756@users.noreply.github.com>
2024-11-14 12:11:42 +0100

    Fix issues with workflow complete handler #4299

--- modules/nextflow/src/main/groovy/nextflow/Session.groovy
+++ modules/nextflow/src/main/groovy/nextflow/Session.groovy
@@ -800,6 +800,7 @@ class Session implements ISession {
             log.debug(SysHelper.dumpThreads())
             // force termination
             notifyError(null)
+            shutdown0()
             ansiLogObserver?.forceTermination()
             executorFactory?.signalExecutors()
             processesBarrier.forceTermination()
